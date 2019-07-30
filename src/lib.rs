f5d8682bf5e20ad44b4769292674e01558182e6d
Vlad Frangu <kingdgrizzle@gmail.com>
2019-07-30 15:56:04 +0300

    src: Small cleanup

--- src/lib.rs
+++ src/lib.rs
@@ -4,22 +4,15 @@ extern crate js_sys;
 #[macro_use(lazy_static)]
 extern crate lazy_static;
 
-use wasm_bindgen::prelude::*;
 mod packer;
 mod constants;
 
-#[wasm_bindgen]
-extern {
-  #[wasm_bindgen(js_namespace = console, js_name = log)]
-  pub fn console_log(s: &str);
-}
-
+use wasm_bindgen::prelude::*;
 use packer::Packer;
 
 #[wasm_bindgen]
 pub fn native_pack(value: JsValue) -> Vec<u8> {
     let mut packer = Packer::new(value);
     let packed = packer.process();
-    console_log(&format!("{:?}", packed));
     packed.to_vec()
 }
